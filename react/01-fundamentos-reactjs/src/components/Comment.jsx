d652f2834f8475bb70a620323a6c6a72df6fab5b
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-02-17 18:14:09 -0300

    02-Estrutura da aplicação - Estilizações do Comentário, Componente Avatar, Aplicando responsividade

--- react/01-fundamentos-reactjs/src/components/Comment.jsx
+++ react/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,11 +1,12 @@
 import { Trash } from '@phosphor-icons/react/dist/ssr'
 import styles from './Comment.module.css'
 import { ThumbsUp } from '@phosphor-icons/react'
+import { Avatar } from './Avatar'
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://avatars.githubusercontent.com/u/38962433?v=4" />
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/38962433?v=4" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
@@ -15,14 +16,16 @@ export function Comment() {
                             <time title='17 de Fevereiro ás 11:23' dateTime="20024-02-17 11:23:00">Cerca de 1h atrás</time>
                         </div>
                         <button title='Deletar comentário'>
-                            <Trash size={20} />
+                            <Trash size={24} />
                         </button>
                     </header>
                     <p>Muito bom Devon, parabéns!! 👏👏</p>
                 </div>
                 <footer>
-                    <ThumbsUp />
-                    Aplaudir <span>20</span>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
                 </footer>
             </div>
         </div>
