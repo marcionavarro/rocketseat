863e4e25b5857a992cfcd5d341a3840d9eaa4598
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-03-16 12:20:42 -0300

    react-redux-zustand [04-Conectando á API] - Criando API de cursos (JSON Server), Carregando dados do curso

--- react-redux-zustand/src/components/Header.tsx
+++ react-redux-zustand/src/components/Header.tsx
@@ -2,6 +2,11 @@ import { useCurrentLesson } from "../store/slices/player";
 
 export function Header() {
     const { currentModule, currentLesson } = useCurrentLesson();
+
+    if(!currentModule || !currentLesson){
+        return null;
+    }
+
     return (
         <div className="flex flex-col gap-1">
             <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
