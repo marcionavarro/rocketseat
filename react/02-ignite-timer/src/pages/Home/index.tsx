00acdba5a44036b8e4a9f22b45cc32a306add0de
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-05-30 17:35:12 -0300

    02-aprofundando-em-hooks [02-Páginas e rotas] - Finalização da Home, Aprimorando os inputs

--- react/02-ignite-timer/src/pages/Home/index.tsx
+++ react/02-ignite-timer/src/pages/Home/index.tsx
@@ -4,6 +4,9 @@ import {
   CountdownContainer,
   FormContainer,
   Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
 } from './styles'
 
 export function Home() {
@@ -12,12 +15,30 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="projeto 1" />
+            <option value="projeto 2" />
+            <option value="projeto 3" />
+            <option value="projeto 4" />
+          </datalist>
 
           <label htmlFor="">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
-          <span>minutos</span>
+          <span>minutos.</span>
         </FormContainer>
 
         <CountdownContainer>
@@ -28,9 +49,9 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <Play /> Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
