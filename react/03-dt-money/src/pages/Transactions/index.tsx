80383173e0f7e59f45abf3edd05a221fbf315857
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-01-17 01:39:50 -0300

    03-http-e-perfomance [01-Estrutura visual] - Componente Header, Componente Summary, Tabela de transacõess, Componente SearchForm

---
+++ react/03-dt-money/src/pages/Transactions/index.tsx
@@ -0,0 +1,60 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight></td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel do apartamento</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 1.200,00
+                </PriceHighlight>
+              </td>
+              <td>Casa</td>
+              <td>27/03/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Computador</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 5.400,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>15/03/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
