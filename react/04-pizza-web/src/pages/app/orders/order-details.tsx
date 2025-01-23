adc8d2ec74a003ce8a17cc595e5b6de196c73a3a
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-01-22 23:58:35 -0300

    04-Integrando Frontend [02-Páginas e componentes(UI)] - Página Listagem de pedidos, Componente Filtros de pedidos, Componente Paginação, Componente Detalhes do pedido

---
+++ react/04-pizza-web/src/pages/app/orders/order-details.tsx
@@ -0,0 +1,101 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+export default function OrderDetails() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Pedido: ad1f2a13f3as21df3a</DialogTitle>
+        <DialogDescription>Detalhes do pedido</DialogDescription>
+      </DialogHeader>
+
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-full bg-slate-400" />
+                  <span className="to-muted-foreground font-medium">
+                    Pendente
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">Cliente</TableCell>
+              <TableCell className="flex justify-end">Marcio Navarro</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">Telefone</TableCell>
+              <TableCell className="flex justify-end">
+                (34) 97777-8888
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">E-mail</TableCell>
+              <TableCell className="flex justify-end">
+                contato@mn.com.br
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">
+                Realizado há
+              </TableCell>
+              <TableCell className="flex justify-end">3 minutos</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead className="text-right">Qtd.</TableHead>
+              <TableHead className="text-right">Preço</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Pizza moda da casa</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 80,00</TableCell>
+              <TableCell className="text-right">R$ 160,00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Pizza 4 queijos</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 50,00</TableCell>
+              <TableCell className="text-right">R$ 100,00</TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>total dos pedidos</TableCell>
+              <TableCell className="text-righ font-me">R$ 260,00</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </DialogContent>
+  )
+}
---
+++ react/04-pizza-web/src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,52 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+import OrderDetails from './order-details'
+
+// export interface OrderTableRowProps {}
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-4 w-4" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        as1df2sa31f2safdas
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Marcio Navarro</TableCell>
+      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-2 w-2" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-2 w-2" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
