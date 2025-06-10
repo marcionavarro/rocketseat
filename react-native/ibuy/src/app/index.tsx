34f29f9850c4d42b339cacc4d18c86c89d5ac3de
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-06-09 23:10:19 -0300

    RN:[01-Tabs e Drawer] - instalando Drawer, Customizando Drawer, Utilizando Drawer Button

--- react-native/ibuy/src/app/index.tsx
+++ react-native/ibuy/src/app/index.tsx
@@ -1,3 +1,4 @@
+import { DrawerToggleButton } from '@react-navigation/drawer'
 import { Link, router } from 'expo-router'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
@@ -8,6 +9,9 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <DrawerToggleButton />
+      </View>
       <TouchableOpacity style={styles.button} onPress={signUp}>
         <Text style={styles.label}>Criar conta</Text>
       </TouchableOpacity>
@@ -18,9 +22,9 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
-    gap: 32
+    gap: 32,
+    padding: 32
   },
   label: {
     fontSize: 16,
@@ -32,5 +36,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     paddingVertical: 10,
     borderRadius: 10
+  },
+  header: {
+    flexDirection: 'row',
+    width: "100%",
+    justifyContent: "flex-end"
   }
 })
