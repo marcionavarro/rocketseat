5ea3da3e4d0ff32063b528d8a28995f3450687db
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-06-09 22:55:28 -0300

    RN:[01-Tabs e Drawer] - Navegação do tipo tabs, Customizando tabs

--- react-native/ibuy/src/app/_layout.tsx
+++ react-native/ibuy/src/app/_layout.tsx
@@ -1,6 +1,29 @@
-import { Stack } from "expo-router"
+import { Tabs } from "expo-router"
+import { MaterialIcons } from "@expo/vector-icons"
 
 export default function Layout() {
-  return <Stack screenOptions={{headerShown: false}}>
-  </Stack>
+  return (
+    <Tabs screenOptions={{
+      headerShown: false,
+      tabBarLabelPosition: "beside-icon",
+      tabBarInactiveTintColor: "#cecece",
+      tabBarActiveTintColor: "#000"
+      /* tabBarShowLabel: false */
+    }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Sign In",
+          tabBarIcon: ({ color }) => <MaterialIcons name="home" size={20} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="sign-up"
+        options={{
+          tabBarLabel: "Sign Up",
+          tabBarIcon: ({ color }) => <MaterialIcons name="person" size={20} color={color} />
+        }}
+      />
+    </Tabs>
+  )
 }
